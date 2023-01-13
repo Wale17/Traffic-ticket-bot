152db3ca42ffc34d572d4fda89b3aaa40db8ec99
Olawale ojo <olawale.ojo@juzto.co>
2023-01-13 12:56:58 +0100

    Trying to solve case sensitive captcha

--- handlers/process.py
+++ handlers/process.py
@@ -1,5 +1,6 @@
 from RPA.Browser.Selenium import Selenium
 from utilities import handler_process_utils
+from service import captcha_solver
 from time import sleep
 
 
@@ -9,22 +10,56 @@ def open_webpage(url):
     if url == "":
         handler_process_utils.raise_error("Harcoded url is empty")
     else:
-        for trial in range(1):
+        for trial in range(2):
             try:
                 browser.open_available_browser(url)
-                print(browser.is_element_visible("//input[@id='numero_identificacion']"))
-                sleep(5)
-                print(browser.is_element_visible("//input[@id='numero_identificacion']"))
-                sleep(5)
-                print(browser.is_element_visible("//input[@id='numero_identificacion']"))
-                sleep(5)
-                print(browser.is_element_visible("//input[@id='numero_identificacion']"))
-                sleep(5)
-                print(browser.is_element_visible("//input[@id='numero_identificacion']"))
+                browser.select_frame("mainFrame")
                 if browser.is_element_visible("//input[@id='numero_identificacion']"):
-                    print(browser.is_element_visible("//input[@id='numero_identificacion']"))
                     break
             except Exception as e:
                 handler_process_utils.handle_error(e)
-        # else:
-        #     handler_process_utils.raise_error("Please check website url")//input[@id='numero_identificacion']
+        else:
+            handler_process_utils.raise_error("Please check website url")
+
+def make_search(doc_type, plate_number, doc_number):
+    if plate_number != "" or plate_number != "null" or plate_number != None:
+        browser.input_text("//input[@id='placa_veh']", plate_number)
+        for i in range(1):
+            try:
+                browser.screenshot("//img[@alt='Security Code']", "captcha.png")
+                captcha_solution = captcha_solver.twocaptcha_solver("captcha.png")
+                browser.input_text("//input[@id='captcha_user_text']", captcha_solution)
+                browser.click_button("//input[@value='Buscar']")
+                sleep(5)
+                if browser.is_element_visible("//font[@color='red']"):
+                    continue
+                elif browser.is_element_visible("//b[@xpath='1']"):
+                    if doc_type == "CC":
+                        browser.select_all_from_list("//select[@name='tipo_documento']", "1")
+                    elif doc_type == "NIT":
+                        browser.select_all_from_list("//select[@name='tipo_documento']", "2")
+                    elif doc_type == "CE":
+                        browser.select_all_from_list("//select[@name='tipo_documento']", "3")
+                    elif doc_type == "TI":
+                        browser.select_all_from_list("//select[@name='tipo_documento']", "4")
+                    elif doc_type == "PA":
+                        browser.select_all_from_list("//select[@name='tipo_documento']", "5")
+                    browser.input_text("//input[@id='numero_identificacion']", doc_number)
+                    for i in range(1):
+                        try:
+                            browser.screenshot("//img[@alt='Security Code']", "captcha.png")
+                            captcha_solution = captcha_solver.twocaptcha_solver("captcha.png")
+                            browser.input_text("//input[@id='captcha_user_text']", captcha_solution)
+                            browser.click_button("//input[@value='Buscar']")
+                            sleep(5)
+                            if browser.is_element_visible("//font[@color='red']"):
+                                continue
+                        except Exception as e:
+                            raise(e)
+                    else:
+                        handler_process_utils.raise_error("couldn't solve captcha")        
+            except Exception as e:
+                raise(e)
+        else:
+            handler_process_utils.raise_error("couldn't solve captcha")
+    
--- task.py
+++ task.py
@@ -1,5 +1,7 @@
 """Template robot with Python."""
 from handlers import process
+from service import captcha_solver
+from time import sleep
 
 
 url = "https://consultas.transitobogota.gov.co:8010/publico/index3.php"
@@ -18,8 +20,11 @@ update =  True
 
 def minimal_task():
     process.open_webpage(url)
+    process.make_search(doc_type, plate_number, doc_number)
+    sleep(10)
     print("Done.")
 
 
 if __name__ == "__main__":
     minimal_task()
+    # captcha_solution = captcha_solver.twocaptcha_solver("captcha.png")
